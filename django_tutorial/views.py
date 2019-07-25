547c1143ef85423272a3cb9a4e6c47aa6305d1e6
hansmu <hans.miikael@gmail.com>
2019-07-25 14:57:40 +0300

    Add example of setting the landing page and form data sending

--- django_tutorial/views.py
+++ django_tutorial/views.py
@@ -2,5 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render # Used to return HTML
 
 
-def hello(request):
+def home(request):
     return render(request, 'home.html')
+
+
+def translate(request):
+    translation_text = request.GET['translationText']
+    return HttpResponse("On the translate page: " + translation_text)
