101bb8ac229267f43c8586c7397712d150d24c73
hansmu <hans.miikael@gmail.com>
2019-07-26 15:19:23 +0300

    Add prefix to URLs for reference via Django

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+# If you have a lot of similar named URLs, then it could be helpful to add a prefix
+app_name = 'accounts'
 
 urlpatterns = [
     url(r'^signup/', views.signup, name='clone-signup'),
