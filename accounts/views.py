15b32ba9d857a69576de56cbe3c39a87767f5853
hansmu <hans.miikael@gmail.com>
2019-07-26 15:00:22 +0300

    Add authentication for duplicate username and add logging in after registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
-            User.objects.create_user(
-                username=request.POST['username'],
-                password=request.POST['password1']
-            )
+            try:
+                User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'User has already been taken!'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(
+                    username=request.POST['username'],
+                    password=request.POST['password1']
+                )
+                login(request, user)
+
             return render(request, 'accounts/signup.html')
         else:
             return render(request, 'accounts/signup.html', {'error': 'Passwords did not match!'})
