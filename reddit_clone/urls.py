5d5c9ddd502f3a468f795febfa5be184a912a6d0
hansmu <hans.miikael@gmail.com>
2019-07-28 13:29:03 +0300

    Add homepage and create post page - includes authenticated endpoint

--- reddit_clone/urls.py
+++ reddit_clone/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^create/', views.create, name='clone-create-post')
+    url(r'^create/', views.create, name='clone-create-post'),
+    url(r'^$', views.homepage, name='clone-homepage')
 ]
--- reddit_clone/views.py
+++ reddit_clone/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+
+from reddit_clone.models import Post
 
 
 @login_required
 def create(request):
-    return render(request, 'reddit/create-post.html')
+    error = ''
+
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['url']:
+            post = Post()
+
+            post.title = request.POST['title']
+            post.url = request.POST['url']
+            post.published_date = timezone.datetime.now()
+            post.author = request.user
+
+            post.save()
+
+            return redirect('clone-homepage')
+        else:
+            error = 'You must include a title and URL to create a post!'
+
+    return render(request, 'reddit/create-post.html', {'error': error})
+
+
+def homepage(request):
+    posts = Post.objects.order_by('score')
+    return render(request, 'reddit/homepage.html', {'posts': posts})
